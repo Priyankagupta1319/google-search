ce60b2fbda2006b4a829edf07a19fed11d1814dd
root <priyankaguptasp123@gmail.com>
2023-08-19 12:40:52 +0000

    why not heading seen

---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,17 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class GoogleSuggestion extends Component {
+    render(){
+        return(
+            <div className="app-container">
+             <div className="container">
+                <h1>heading</h1>
+             </div>
+            </div>
+        )
+    }
+}
+
+default export GoogleSuggestion
